5a56f8da63b39a53482ee39b86f002ecc1134a9f
niteshgiri-7 <niteshgiri1717@gmail.com>
2025-03-10 13:18:59 +0545

    update response interceptor to run only once

--- src/api/axiosInterceptor.ts
+++ src/api/axiosInterceptor.ts
@@ -21,13 +21,18 @@ Axios.interceptors.response.use(
       ICustomAxiosRequestConfig
     >
   ) => {
+    console.log("error", error);
     if (!error.config) return Promise.reject(error);
 
     const originalRequest: ICustomAxiosRequestConfig = error.config;
+    console.log("originalRequest", originalRequest);
+    originalRequest._retry = false;
+    console.log("retry", originalRequest._retry);
 
     if (
-      (error.response && error.response.status === 401) ||
-      (error.response?.data.action === "refresh" && !originalRequest._retry)
+      error.response?.status === 401 &&
+      error.response?.data?.action === "refresh" &&
+      !originalRequest._retry
     ) {
       originalRequest._retry = true;
 
@@ -36,7 +41,7 @@ Axios.interceptors.response.use(
         if (!user) throw new Error("No User logged in!");
 
         const newToken = await user.getIdToken(true);
-
+        console.log("newToken", newToken);
         await Axios.post(
           "/user/refresh-token",
           { rememberMe },
@@ -51,6 +56,7 @@ Axios.interceptors.response.use(
         return Axios(originalRequest);
       } catch (refreshError) {
         console.log(refreshError);
+        return Promise.reject(refreshError);
       }
     }
     return Promise.reject(error);
